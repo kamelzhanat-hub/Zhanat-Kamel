3c59662a4b11b2adcbdb032e7dc03942e326127e
kamelzhanat-hub <zanatz338@gmail.com>
2026-06-12 23:22:02 +0500

    Add files via upload

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,7 @@
+#this is a comment 
+print("Hello World")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,38 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#integers
+a = 1
+b = 35656222554887711
+c = -3255522
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#float
+q = 1.10
+w = 1.0
+e = -35.59
+
+print(type(q))
+print(type(w))
+print(type(e))
+
+r = 35e3
+t = 12E4
+y = -87.7e100
+
+print(type(r))
+print(type(t))
+print(type(y))
+
+#complex
+u = 3+5j
+i = 5j
+o = -5j
+
+print(type(u))
+print(type(i))
+print(type(o))
