81d5017ad75357b82571b7ee001803de557860ec
KamelZhanat <kasenovaaltynaj172@gmail.com>
2026-06-12 22:23:29 +0500

    Practice1 Python basics examples

---
+++ casting.py
@@ -0,0 +1,5 @@
+#Specify a Variable Type
+x = int(1)   # x will be 1
+y = float(2.8)   # y will be 2.8
+z = str(3.0)  # z will be '3.0'
+
---
+++ strings.py
@@ -0,0 +1,18 @@
+print("Hello")
+print('Hello')
+
+#quotes inside quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#Assign String to a Variable
+a = "Hello"
+print(a)
+
+#Multiline Strings
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
