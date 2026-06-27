42ca41e6a08590b5371439275c1d633e1be9dd56
kamelzhanat-hub <zanatz338@gmail.com>
2026-06-27 16:01:39 +0500

    Update move_files.py

---
+++ Practice6/directory_management/move_files.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+
+source_file = "../file_handling/sample.txt"
+destination_dir = "test_dir"
+
+if os.path.exists(source_file):
+    shutil.move(source_file, destination_dir)
+    print("File moved successfully.")
+else:
+    print("Source file not found.")
